e7a81efe6eb247188a521fd57e27c436422aeaf0
Harshit Garg <ggharshit1@gmail.com>
2023-07-07 18:07:46 +0530

    Project Setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (
+        <div
+            className="p-4"
+        >
+            <p
+                className="text-base"
+            >
+                Hello Admin Dashboard
+            </p>
+        </div>
+    )
+}
