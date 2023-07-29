a260f3d3b6bd7580430b43a89d69f227c138e63f
Harshit Garg <ggharshit1@gmail.com>
2023-07-29 15:05:04 +0530

    HomePage Added

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,25 +1,135 @@
-import prismadb from "@/lib/prismadb"
+import {
+    CreditCard,
+    IndianRupee,
+    Package
+} from "lucide-react"
 
-interface DashboardPageProps{
+import { formatter } from "@/lib/utils"
+import { getTotalRevenue } from "@/actions/get-total-revenue"
+import { getSalesCount } from "@/actions/get-sales-count"
+import { getStockCount } from "@/actions/get-stock-count"
+import { getGraphRevenue } from "@/actions/get-graph-revenue"
+
+import Heading from "@/components/ui/heading"
+import { Separator } from "@/components/ui/separator"
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    CardTitle
+} from "@/components/ui/card"
+import Overview from "@/components/overview"
+
+interface DashboardPageProps {
     params: { storeId: string }
 }
 
-const DashboardPage:React.FC<DashboardPageProps> = async ({
+const DashboardPage: React.FC<DashboardPageProps> = async ({
     params
 }) => {
 
-    const store = await prismadb.store.findFirst({
-        where:{
-            id: params.storeId
-        }
-    })
+    const totalRevenue = await getTotalRevenue(params.storeId)
+    const salesCount = await getSalesCount(params.storeId)
+    const stockCount = await getStockCount(params.storeId)
+    const graphData = await getGraphRevenue(params.storeId)
 
     return (
-        <>
-            <div>
-                THIS IS A DASHBOARD FOR {store?.name}
+        <div
+            className="flex flex-col"
+        >
+            <div
+                className="flex-1 space-y-4 p-8 pt-6 "
+            >
+                <Heading
+                    title="Dashboard"
+                    description="Overview of the store."
+                />
+                <Separator />
+                <div
+                    className="grid gap-4 grid-cols-3 "
+                >
+                    <Card>
+                        <CardHeader
+                            className="flex flex-row items-center justify-between space-y-0 pb-2 "
+                        >
+                            <CardTitle
+                                className="text-sm font-medium "
+                            >
+                                Total Revenue
+                            </CardTitle>
+                            <IndianRupee
+                                className="h-4 w-4 text-muted-foreground "
+                            />
+                        </CardHeader>
+                        <CardContent>
+                            <div
+                                className="text-2xl font-bold "
+                            >
+                                {formatter.format(totalRevenue)}
+                            </div>
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardHeader
+                            className="flex flex-row items-center justify-between space-y-0 pb-2 "
+                        >
+                            <CardTitle
+                                className="text-sm font-medium "
+                            >
+                                Sales
+                            </CardTitle>
+                            <CreditCard
+                                className="h-4 w-4 text-muted-foreground "
+                            />
+                        </CardHeader>
+                        <CardContent>
+                            <div
+                                className="text-2xl font-bold "
+                            >
+                                +{salesCount}
+                            </div>
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardHeader
+                            className="flex flex-row items-center justify-between space-y-0 pb-2 "
+                        >
+                            <CardTitle
+                                className="text-sm font-medium capitalize "
+                            >
+                                Products in stock
+                            </CardTitle>
+                            <Package
+                                className="h-4 w-4 text-muted-foreground "
+                            />
+                        </CardHeader>
+                        <CardContent>
+                            <div
+                                className="text-2xl font-bold "
+                            >
+                                {stockCount}
+                            </div>
+                        </CardContent>
+                    </Card>
+                </div>
+                <Card
+                    className="col-span-4 "
+                >
+                    <CardHeader>
+                        <CardTitle>
+                            Overview
+                        </CardTitle>
+                    </CardHeader>
+                    <CardContent
+                        className="pl-2 "
+                    >
+                        <Overview
+                            data={graphData}
+                        />
+                    </CardContent>
+                </Card>
             </div>
-        </>
+        </div>
     )
 }
 
